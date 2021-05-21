1c4b043a8f7b72cd2cb83d31e0baffa7b65c5997
jack98 <jack98@DESKTOP-8NAJLJD>
2021-05-21 14:42:26 +0800

    Controller mapping to /api

--- src/main/java/com/example/demo/web/HelloControler.java
+++ src/main/java/com/example/demo/web/HelloControler.java
@@ -8,21 +8,17 @@
 package com.example.demo.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloControler {
 
+//    @RequestMapping(value="/say", method=RequestMethod.GET)
     @RequestMapping("/say")
     public String say() {
         return "Hello Sprig Boot";
     }
-    
-    @RequestMapping(value="/sayWithMethod", method=RequestMethod.GET)
-    /* NOTE: @RequestMapping(value="/sayWithMethod", method=RequestMethod.GET) equal to @GetMapping("sayWithMethod") */
-    public String sayWithMethod() {
-        return "Hello Sprig Boot with RequestMethod.GET";
-    }
+
 
 }
