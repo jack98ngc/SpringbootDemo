3f376baad9bc90a57022fc88394f5eb4deb5c701
jack98 <jack98@DESKTOP-8NAJLJD>
2021-05-24 15:36:03 +0800

    1-25_表單處理-post請求與redirect

--- src/main/java/com/example/demo/web/BookController.java
+++ src/main/java/com/example/demo/web/BookController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.domain.Book;
 import com.example.demo.service.BookService;
@@ -39,5 +40,16 @@ public class BookController {
 //        }
         model.addAttribute("book", book);
         return "book";
+    }
+    
+    @GetMapping("/books/input")
+    public String inputPage() {
+        return "input";
+    }
+    
+    @PostMapping("/books")
+    public String post(Book book) {
+        bookService.save(book);
+        return "redirect:/books";
     }
 }
